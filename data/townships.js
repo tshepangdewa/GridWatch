debdb200a717a241b05aa153ea20660914097d9d
Tshepang Dewa <tshepangdewa@gmail.com>
2026-09-09 22:38:43 +0200

    Add files via upload

---
+++ data/townships.js
@@ -0,0 +1,122 @@
+export const townships = [
+    {
+        name: "Cowdray Park",
+        latitude: -20.08653,
+        longitude: 28.51913,
+        status: "ON"
+    },
+    {
+        name: "Nkulumane",
+        latitude: -20.18016,
+        longitude: 28.51523,
+        status: "OFF"
+    },
+    {
+        name: "Luveve",
+        latitude: -20.09923,
+        longitude: 28.50843,
+        status: "ON"
+    },
+    {
+        name: "Emakhandeni",
+        latitude: -20.11810,
+        longitude: 28.52628,
+        status: "OFF"
+    },
+    {
+        name: "Entumbane",
+        latitude: -20.12528,
+        longitude: 28.54389,
+        status: "ON"
+    },
+    {
+        name: "Pumula",
+        latitude: -20.14075,
+        longitude: 28.48112,
+        status: "OFF"
+    },
+    {
+        name: "Lobengula",
+        latitude: -20.12640,
+        longitude: 28.51720,
+        status: "ON"
+    },
+    {
+        name: "Nketa",
+        latitude: -20.19750,
+        longitude: 28.531944,
+        status: "OFF"
+    },
+    {
+        name: "Magwegwe",
+        latitude: -20.13723,
+        longitude: 28.50828,
+        status: "ON"
+    },
+    {
+        name: "Emganwini",
+        latitude: -20.22044,
+        longitude: 28.51211,
+        status: "OFF"
+    },
+    {
+        name: "Tshabalala",
+        latitude: -20.17525,
+        longitude: 28.53435,
+        status: "ON"
+    },
+    {
+        name: "Sizinda",
+        latitude: -20.17350,
+        longitude: 28.54345,
+        status: "OFF"
+    },
+    {
+        name: "Njube",
+        latitude: -20.13546,
+        longitude: 28.53061,
+        status: "ON"
+    },
+    {
+        name: "Makokoba",
+        latitude: -20.14195,
+        longitude: 28.57514,
+        status: "OFF"
+    },
+    {
+        name: "Mzilikazi",
+        latitude: -20.12743,
+        longitude: 28.56748,
+        status: "ON"
+    },
+    {
+        name: "Matshobana",
+        latitude: -20.13806,
+        longitude: 28.55250,
+        status: "OFF"
+    },
+    {
+        name: "Gwabalanda",
+        latitude: -20.11667,
+        longitude: 28.50173,
+        status: "ON"
+    },
+    {
+        name: "Barbourfields",
+        latitude: -20.12889,
+        longitude: 28.57000,
+        status: "OFF"
+    },
+    {
+        name: "Nguboyenja",
+        latitude: -20.13863,
+        longitude: 28.56443,
+        status: "ON"
+    },
+    {
+        name: "Mabutweni",
+        latitude: -20.14250,
+        longitude: 28.53167,
+        status: "OFF"
+    }
+];
